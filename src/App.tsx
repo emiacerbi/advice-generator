93f9f737f1bb6fd868dc681f3c8a49b9a3df723a
Emiliano Acerbi <eacerbip@gmail.com>
2022-12-13 10:16:50 -0300

    small delay when refetching and box shadow for card

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,40 @@ import Die from './components/Die'
 import Divider from './components/Divider'
 import Wrapper from './components/Wrapper'
 import Spinner from './components/Spinner'
+import { useState } from 'react'
 
 function App() {
-  const { data, refetch, isRefetching, isFetching, error } = useQuery({
+  const [isRefetching, setIsRefetching] = useState(false)
+  const { data, refetch, isLoading, error } = useQuery({
     queryKey: ['quote'],
     queryFn: getQuote,
+    refetchOnWindowFocus: false,
   })
 
   const handleClick = () => {
     refetch()
+    setIsRefetching(true)
+    setTimeout(() => {
+      setIsRefetching(false)
+    }, 1000)
   }
 
   if (error) console.error(error)
 
   return (
     <Wrapper>
-      <main className="max-w-[540px] grid gap-8  text-center relative font-primary py-14 px-6 sm:px-12 sm bg-neutral-800 rounded-2xl">
+      <main className="max-w-[540px] shadow-lg grid gap-8  text-center relative font-primary py-14 px-6 sm:px-12 sm bg-neutral-800 rounded-2xl">
         <p className="text-primary-400 tracking-[.25rem] text-sm">
           ADVICE #117
         </p>
         <div className="text-primary-200 text-quote grid place-content-center">
-          {isFetching || isRefetching ? (
-            <Spinner />
-          ) : (
-            <p>{data?.slip.advice}</p>
-          )}
+          {isLoading || isRefetching ? <Spinner /> : <p>{data?.slip.advice}</p>}
         </div>
 
         <Divider />
         <button
           onClick={handleClick}
-          disabled={isFetching || isRefetching}
+          disabled={isLoading || isRefetching}
           className="absolute disabled:cursor-auto cursor-pointer hover:shadow-primary transition-shadow duration-200 -bottom-8 left-1/2 -translate-x-1/2 bg-primary-400 p-5 w-fit mx-auto rounded-full"
         >
           <Die />
